1bb3a080b5dd869d2c9ed06b08380f04d4b720cc
XThexLonexWolfX <yorben_joosen01@hotmail.com>
2022-01-22 16:09:08 +0100

    Updated help command

--- Useful/helpcommands.py
+++ Useful/helpcommands.py
@@ -8,5 +8,6 @@ async def helpcommands(ctx):
     embed.add_field(name='Music commands', value=config_file.help_music, inline=False)
     embed.add_field(name='Useful commands', value=config_file.help_useful, inline=False)
     embed.add_field(name='Random commands', value=config_file.help_random, inline=False)
+    embed.add_field(name='Admin commands', value=config_file.help_admin, inline=False)
     embed.set_footer(text='This bot was made by Ouzen#2099 also known as XThexLonexWolfX or Yorben Joosen')
     await ctx.send(embed=embed)
